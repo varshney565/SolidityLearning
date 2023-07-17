ca97b43acfc0cd4e8353ca8c2a3d13d42204cab1
varshney565 <uietcse1923shivamvarshney@kuk.ac.in>
2023-07-17 15:49:09 +0530

    structure done

---
+++ Structure.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+struct Student{
+    uint roll;
+    string name;
+}
+
+contract Demo{
+    Student public s1;
+    constructor(uint _roll,string memory _name){
+        s1.roll = _roll;
+        s1.name = _name;
+    }
+
+    function change(uint _roll,string memory _name) public{
+        // s1.roll = _roll;
+        // s1.name = _name;
+
+        Student memory newStudent = Student({
+            roll : _roll,
+            name : _name
+        }); 
+        s1 = newStudent;
+    }
+}
