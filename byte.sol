98dc2c91240bc513d71a658fc6ddb5889143b6e2
varshney565 <uietcse1923shivamvarshney@kuk.ac.in>
2023-07-17 14:28:45 +0530

    uploading code

--- byte.sol
+++ byte.sol
@@ -7,10 +7,16 @@ contract local {
     bytes3 public c = "ABC"; //3 bytes array
     bytes2 public b; //2 bytes array
 
-    //NOTE : byte arrays are immutable.
     //dynamic size array
     bytes public a = new bytes(6);
+    bytes public d;
     constructor(){
         a = "abcdef";
+    }
+
+    function setter() public{
+       d = "fsjdfjhfajhf";
+       d[0] = 'm';
+       a[0] = 'p';
     }
 }
